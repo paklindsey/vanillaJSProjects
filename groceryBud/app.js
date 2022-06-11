7b0772c37e499f757ca2d91fca8ff5e21a407c24
Lindsey <paklindsey@gmail.com>
2022-06-10 23:28:13 -0400

    js

--- groceryBud/app.js
+++ groceryBud/app.js
@@ -1,10 +1,45 @@
 // ****** SELECT ITEMS **********
+const alert = document.querySelector(".alert");
+const form = document.querySelector(".grocery-form");
+const submitBtn = document.querySelector(".submit-btn");
+const container = document.querySelector(".grocery-container");
+const list = document.querySelector(".grocery-list");
+const clearBtn = document.querySelector(".clear-btn");
+const grocery = document.getElementById("grocery");
 
 // edit option
+let editElement;
+let editFlag = false;
+let editID = "";
 
 // ****** EVENT LISTENERS **********
+//submit form
+form.addEventListener("submit", addItem);
 
 // ****** FUNCTIONS **********
+function addItem(e) {
+  e.preventDefault();
+  const value = grocery.value;
+  const id = new Date().getTime().toString();
+  if (value && !editFlag) {
+    console.log("add item");
+  } else if (value && editFlag) {
+    console.log("editing");
+  } else {
+    displayAlert("please enter value", "danger");
+  }
+}
+
+// display alert
+function displayAlert(text, action) {
+  alert.textContent = text;
+  alert.classList.add(`alert-${action}`);
+  //remove alert
+  setTimeout(function () {
+    alert.textContent = "";
+    alert.classList.remove(`alert-${action}`);
+  }, 2000);
+}
 
 // ****** LOCAL STORAGE **********
 
--- groceryBud/log.md
+++ groceryBud/log.md
@@ -1,6 +1,7 @@
 **youtube video:** https://www.youtube.com/watch?v=3PHXvlpOkf4&t=14s&ab_channel=freeCodeCamp.org
 
-| date | timestamp | machine |
-| ---- | --------- | ------- |
-| 5.31 | 5:00:00   | work    |
-| 6.1  | 6:26:18   | work    |
+| date | timestamp | machine  |
+| ---- | --------- | -------- |
+| 5.31 | 5:00:00   | work     |
+| 6.1  | 6:26:18   | work     |
+| 6.10 | 8:23:56   | personal |
